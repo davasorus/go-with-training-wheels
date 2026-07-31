6f82e30954885e4ceceea66c7b9f18026b7fa076
Sean Davitt <davitts2@gmail.com>
2026-07-31 12:28:47 -0400

    who needs AI when you need math\rand

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,6 +1,9 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
@@ -10,10 +13,22 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	//:= is a short variable declaration, which declares and initializes the variable in one line.
 	//can also be broken up into multiple lines for readability
 	//var message string
 	//message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Hello, %v! How are you?",
+		"Great to see you, %v!",
+		"Hey, %v! Nice to meet you!",
+	}
+	// Return a randomly selected format.
+	return formats[rand.Intn(len(formats))]
 }
